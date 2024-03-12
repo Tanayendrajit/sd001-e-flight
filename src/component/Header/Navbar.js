5b88640509b59511d8a88cba7f5928db2e2060f0
Tanayendrajit <tanayendrajit98@gmail.com>
2024-03-12 11:44:14 +0530

    Files Updated

--- src/component/Header/Navbar.js
+++ src/component/Header/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../img/logo.png';
+import UserIcon from '../../img/usericon.png';
 
 function Header () {
     return (
@@ -9,21 +10,43 @@ function Header () {
                     <a className='navbar-brand' href="#">
                         <img src={Logo} style={{height:60, width:"100%"}} alt='' />
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                        </li>
-                        <li className="nav-item">
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Check-in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Bookings</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login/Signup</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
         </>
     );
 }
 
 export default Header;
+
+
+/*
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    
+*/
